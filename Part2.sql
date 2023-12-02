c9aaa5f22ce0b456b30fb2e16fbee1b01042bc18
Christy <christy@zipcodersmbp90.mynetworksettings.com>
2023-12-02 12:58:38 -0500

    complete 5 answer at part2

--- Part2.sql
+++ Part2.sql
@@ -4,3 +4,7 @@
  SELECT * FROM pokemons where id = 45;
  --3
   SELECT COUNT(*) as number_of_pokemon FROM pokemons;
+ --4
+ SELECT COUNT(*) as number_of_types FROM types;
+ --5
+  SELECT COUNT(*) as number_secondary_type FROM pokemons Where secondary_type IS NOT NULL;
